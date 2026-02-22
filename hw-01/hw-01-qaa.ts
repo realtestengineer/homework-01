f2d54d50504da082ea6e94c3131493b8274f9c41
RilwanAminu <rilwana61@gmail.com>
2026-02-22 15:23:55 +0000

    Initial commit

---
+++ hw-01/hw-01-qaa.ts
@@ -0,0 +1,17 @@
+// // Step 1: Declare Variable
+// let age : number = 29 ;
+// let firstLetter : string = "R" ;
+// let score : number = 10 ;
+// let initial : string = "RA" ;
+
+
+//Step 2: Modify the Variables
+let age : number = 11 ;
+let firstLetter : string = "A" ;
+let score : number = 5 ;
+let initial : string = "NM" ;
+
+console.log(age);
+console.log(firstLetter);
+console.log(score);
+console.log(initial);
